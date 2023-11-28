073df247e044e1b8219ccee6f3656305554464fc
Simon Mathis <simon.mathis@gmail.com>
2023-11-28 13:58:25 +0000

    fix: add decimals to ensure proper division

--- plotstyle/constants.py
+++ plotstyle/constants.py
@@ -6,8 +6,8 @@ PLOTSTYLE_DATA_DIR = os.path.join(PLOTSTYLE_DIR, 'data')
 
 # Constants for unit conversions
 INCH_TO_CM = 2.54
-CM_TO_INCH = 1 / INCH_TO_CM
-INCH_TO_PT = 72
-PT_TO_INCH = 1 / INCH_TO_PT
+CM_TO_INCH = 1.0 / INCH_TO_CM
+INCH_TO_PT = 72.0
+PT_TO_INCH = 1.0 / INCH_TO_PT
 CM_TO_PT = CM_TO_INCH * INCH_TO_PT
-PT_TO_CM = 1 / CM_TO_PT
+PT_TO_CM = 1.0 / CM_TO_PT
