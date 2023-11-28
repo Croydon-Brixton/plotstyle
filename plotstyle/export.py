c24326432cb308cc47ee962f36fc78b6644eb8bb
Simon Mathis <simon.mathis@gmail.com>
2023-11-28 13:57:02 +0000

    feat: add save functionality

---
+++ plotstyle/export.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+import datetime
+import os
+import matplotlib.pyplot as plt
+import re
+
+_VALID_EXTENSIONS = ('pdf', 'png', 'jpg', 'jpeg', 'svg', 'eps')
+
+def save_timestamped_figure(
+        name: str, 
+        save_dir: str | None = None,
+        fig: plt.Figure | None = None,
+        file_types: str | list[str] = ["pdf", "png"],
+        date_format: str = 'v%Y-%m-%d-%H:%M',
+        **savefig_kwargs
+):
+    """Save a figure with a time stamp in the file name."""
+    if isinstance(file_types, str):
+        file_types = [file_types]
+
+    # check that all file types are valid
+    for file_type in file_types:
+        if file_type not in _VALID_EXTENSIONS:
+            raise ValueError(f'Unrecognized file type {file_type}. Valid file types are {_VALID_EXTENSIONS}.')
+        
+    # if save_dir is given, check that the name is not an absolute path
+    if save_dir:
+        if os.path.isabs(name):
+            raise ValueError('If `save_dir` is given, name must be a relative path.')
+        # Turn the name into an absolute path
+        name = os.path.join(save_dir, name)
+
+    # remove any file extensions from the name
+    name = re.sub(f'\.({"|".join(_VALID_EXTENSIONS)})$', '', name)
+
+    # get the current time stamp
+    timestamp = datetime.datetime.now().strftime(date_format)
+
+    # save the figure(s)
+    for file_type in file_types:
+        if fig is None:
+            plt.savefig(f'{name}_{timestamp}.{file_type}', bbox_inches='tight', pad_inches=0.04, **savefig_kwargs)
+        else:
+            fig.savefig(f'{name}_{timestamp}.{file_type}', bbox_inches='tight', pad_inches=0.04, **savefig_kwargs)
