35cda5d5b63a5c11c89deb685c9d87c7e75e1ce4
Simon Mathis <simon.mathis@gmail.com>
2023-11-28 14:45:29 +0000

    feat: add further default widths

--- plotstyle/size.py
+++ plotstyle/size.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 __all__ = [
     "GOLDEN_RATIO",
-    "TEXTWIDTHS",
+    "WIDTH",
     "convert_unit",
     "get_dim",
     "set_dim",
@@ -14,22 +14,13 @@ __all__ = [
 
 GOLDEN_RATIO = (5**0.5 - 1) / 2
 
-
-@dataclass
-class TEXTWIDTHS:
-    # Determined via: https://tex.stackexchange.com/a/39384
-    #  all textwidths measured in pt
-    _units: str = "pt"
-    MRES_REPORT: float = 398.3386
-    PHD_THESIS: float = 455.24411
-    NEURIPS_ARTICLE: float = 397.48499
-    LATEX_ARTICLE: float = 345.0
-
-
 _UNIT_CONVERSIONS = {
     ("in", "cm"): 2.54,
-    ("in", "pt"): 72.0,
-    ("cm", "pt"): 72.0 / 2.54,
+    ("in", "pt"): 72.27,
+    ("cm", "pt"): 72.27 / 2.54,
+    ("mm", "pt"): 72.27 / 25.4,
+    ("in", "mm"): 25.4,
+    ("cm", "mm"): 10.0,
 }
 
 
@@ -43,8 +34,30 @@ def convert_unit(from_unit: str, to_unit: str) -> float:
         raise ValueError(f"Conversion {from_unit} to {to_unit} not implemented.")
 
 
+@dataclass
+class WIDTH:
+    # Determined via: https://tex.stackexchange.com/a/39384
+    #  all textwidths measured in pt
+    _units: str = "pt"
+    # Latex default
+    latex_default_article: float = 345.0
+    # Cambridge thesis guidelines
+    mres_report: float = 398.3386
+    phd_thesis: float = 455.24411
+    # Machine learning conference guidelines
+    neurips_article: float = 397.48499
+    # https://www-nature-com.ezp.lib.cam.ac.uk/nature/for-authors/final-submission
+    nature_column: float = 89.0 * convert_unit("mm", "pt")
+    nature_2column: float = 183.0 * convert_unit("mm", "pt")
+    nature_page: float = 247.0 * convert_unit("mm", "pt")
+    # https://www.science.org/content/page/instructions-preparing-initial-manuscript
+    science_column: 5.7 * convert_unit("cm", "pt")
+    science_2column: float = 12.1 * convert_unit("cm", "pt")
+    science_3column: float = 18.4 * convert_unit("cm", "pt")
+
+
 def get_dim(
-    width: float = TEXTWIDTHS.LATEX_ARTICLE,
+    width: float = WIDTH.latex_default_article,
     width_unit: str = "pt",
     fraction_of_line_width: float = 1.0,
     ratio: float = GOLDEN_RATIO,
@@ -55,7 +68,7 @@ def get_dim(
 
     Args:
         width (float): Textwidth of the document to make fontsizes match, in the
-            units given in `width_unit`. Defaults to `TEXTWIDTHS.LATEX_ARTICLE`
+            units given in `width_unit`. Defaults to `WIDTH.latex_default_article`
             (345.0pt)
         width_unit (str, optional): Units of width. Defaults to "pt".
         fraction_of_line_width (float, optional): Fraction of the document width
